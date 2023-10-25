21e64f3bae17ff2ccb6aadb4cf656c506fe83704
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-10-25 13:30:46 -0300

    mongodb agregado

--- src/app.js
+++ src/app.js
@@ -4,11 +4,35 @@ const http = require('http');
 const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-const ProductManager = require('./ProductManager');
+const mongoose = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+// Conecta tu aplicación a MongoDB
+mongoose.connect('mongodb://localhost/tu_base_de_datos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conexión a MongoDB exitosa');
+});
+
+// Define un modelo de datos utilizando Mongoose
+const productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  thumbnail: String,
+  code: String,
+  stock: Number,
+});
+
+const Producto = mongoose.model('Producto', productSchema);
 
 const filePath = 'products.json';
 
@@ -19,82 +43,53 @@ app.set('view engine', 'handlebars');
 // Configurar el uso de bodyParser para analizar JSON
 app.use(bodyParser.json());
 
-// Crear una instancia de ProductManager
-const productManager = new ProductManager();
-
 // Ruta para cargar la página principal
 app.get('/', (req, res) => {
-  const productsList = productManager.getProducts();
-  res.render('home', { productsList });
+  // Aquí puedes usar Mongoose para obtener productos desde la base de datos
+  Producto.find({}, (err, products) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener productos');
+    } else {
+      res.render('home', { productsList: products });
+    }
+  });
 });
 
 // Ruta para mostrar la vista de productos en tiempo real
 app.get('/realtimeproducts', (req, res) => {
-  const productsList = productManager.getProducts();
-  res.render('realTimeProducts', { productsList });
+  // Aquí puedes usar Mongoose para obtener productos desde la base de datos
+  Producto.find({}, (err, products) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener productos');
+    } else {
+      res.render('realTimeProducts', { productsList: products });
+    }
+  });
 });
-
 
 // Ruta para agregar un producto mediante un formulario
 app.post('/add-product', (req, res) => {
-  const newProduct = productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc1223t34t3",
-    25
-  );
-  
-  productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc12ewer3",
-    25
-  );
-  
-  productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc12werwr3",
-    25
-  );
-  
-  productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc12werw3",
-    25
-  );
-  
-  productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc12ewett3",
-    25
-  );
-  
-  productManager.addProduct(
-    "Producto Prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc12444tgg3",
-    25
-  );
+  const newProduct = new Producto({
+    title: "Producto Prueba",
+    description: "Este es un producto prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc1223t34t3",
+    stock: 25,
+  });
 
-  // Emitir evento a través de WebSocket
-  io.emit('productAdded', newProduct);
-
-  res.redirect('/');
+  newProduct.save((err, product) => {
+    if (err) {
+      console.error(err);
+      res.status(400).json({ error: err.message });
+    } else {
+      // Emitir evento a través de WebSocket
+      io.emit('productAdded', product);
+      res.redirect('/');
+    }
+  });
 });
 
 // Configurar WebSockets
@@ -107,12 +102,15 @@ io.on('connection', (socket) => {
 });
 
 // Obtener la lista de productos (incluyendo el producto recién agregado)
-const productsList = productManager.getProducts();
-
-// Ver la lista de productos
-console.log("Lista de productos:");
-productsList.forEach((product) => {
-  console.log(product);
+Producto.find({}, (err, products) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Lista de productos:");
+    products.forEach((product) => {
+      console.log(product);
+    });
+  }
 });
 
 // Iniciar el servidor HTTP
